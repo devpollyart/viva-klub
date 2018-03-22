ba5bdd3827466c634dd3ee7859007184c45ab508
dev <dev@pollyart.pl>
2018-03-22 08:16:39 +0100

    prawie skonczona responsywnosc

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,22 @@
 (function($){
     $(function(){
-        
+        var popup = $('.list__item'),
+            overlayDiv = $('.popup'),
+            picture;
+
+        popup.on('click','img', function(){
+            overlayDiv.css('block','none')
+            overlayDiv.fadeIn(600);
+            picture = $(this).attr('src')
+            picture = picture.slice(0,8) + 'B' + picture.slice(8, picture.length);
+            overlayDiv.find('img').attr('src', picture);
+            
+        });
 
 
+        overlayDiv.click(function(){
+            overlayDiv.fadeOut(600);
+        })
 
     });
 })(jQuery);
