21a59a516b673de67eb20438e26cd30629791025
dev <dev@pollyart.pl>
2018-03-23 13:32:09 +0100

    s

--- js/custom.js
+++ js/custom.js
@@ -13,9 +13,66 @@
             
         });
 
-
         overlayDiv.click(function(){
             overlayDiv.fadeOut(600);
+        })
+
+        var fooSpo = function () {
+            var textReplace = $(this).html();
+            var lettersToReplace = ["a", "i", "o", "u", "w", "z", "A", "I", "O", "U", "W", "Z"];
+            var arrayLength = lettersToReplace.length;
+            for (var i = 0; i < arrayLength; i++) {
+                var textSplit = textReplace.split(' ' + lettersToReplace[i] + ' ');
+                var textReplace = textSplit.join(' ' + lettersToReplace[i] + '&nbsp;');
+            }
+            $(this).empty();
+            $(this).html(textReplace);
+        }
+        var initSpo = function () {
+            $('p').each(fooSpo);
+            $('h2').each(fooSpo);
+            $('h3').each(fooSpo);
+            $('h4').each(fooSpo);
+        }
+
+        initSpo();
+        
+        $('#form--one').on('submit', function(e){
+            e.preventDefault();
+            var data = $(this).serialize();
+            $.ajax({
+                method: "POST",
+                data: data,
+                url: "/send.php"
+            }).done(function(response){
+                if(response === 'Wysłano'){
+                   $('input').val("");
+                //    $('textarea').val("");
+                   $('input:submit').css('background','#F37100').val("Wysłano");
+                } else {
+                    console.log('');
+                }
+            })
+        })
+
+        $('.formError').html('Pole wymagane');
+        
+        $('#form--two').on('submit', function(e){
+            e.preventDefault();
+            var data = $(this).serialize();
+            $.ajax({
+                method: "POST",
+                data: data,
+                url: "/send.php"
+            }).done(function(response){
+                if(response === 'Wysłano'){
+                   $('input').val("");
+                //    $('textarea').val("");
+                   $('input:submit').css('background','#F37100').val("Wysłano");
+                } else {
+                    console.log('');
+                }
+            })
         })
 
     });
